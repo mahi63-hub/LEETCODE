f150ba766b34e1d23ba45658db086450fe601f9d
MAHI RALI <23MH1A4963@acoe.edu.in>
2025-09-17 11:34:55 +0530

    Time: 11 ms (23.85%) | Memory: 45.1 MB (90.76%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int i:nums){
+            if(set.contains(i)){
+                set.remove(i);
+            }else{
+                set.add(i);
+            }
+        }
+        return set.iterator().next();
+    }
+}
