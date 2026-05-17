3ded9e2a146753bbdb32236e2105409858d5aabb
Rali Mahi <23MH1A4963@acoe.edu.in>
2026-05-17 15:26:33 +0530

    Time: 2 ms (82.42%) | Memory: 52.1 MB (54.97%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        if(start<0 || start>=arr.length || arr[start]<0){
+            return false;
+        }
+
+        if(arr[start]==0){
+            return true;
+        }
+
+        int index=arr[start];
+        arr[start]=-arr[start];
+        return canReach(arr, start-index) || canReach(arr, start+index);
+    }
+}
