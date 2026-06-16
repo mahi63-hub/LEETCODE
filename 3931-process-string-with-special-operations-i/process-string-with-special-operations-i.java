fd8bfa3e9dc4aeb485c08276804f1a079ebefa4d
Rali Mahi <23MH1A4963@acoe.edu.in>
2026-06-16 10:05:40 +0530

    Time: 4 ms (76.47%) | Memory: 55 MB (80.88%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder st= new StringBuilder();
+        for(char c: s.toCharArray()){
+            if(Character.isLetter(c)){
+                st.append(c);
+            }else if(c=='#'){
+                st.append(st);
+            }else if(c=='%'){
+                st.reverse();
+            }else if(c=='*' && st.length()!=0){
+                st.deleteCharAt(st.length()-1);
+            }
+        }
+        return st.toString();
+    }
+}
