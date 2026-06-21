f8fa46df1545be1d1cab1b5699c8f879d2dd0aa1
Rali Mahi <23MH1A4963@acoe.edu.in>
2026-06-21 20:34:22 +0530

    Time: 38 ms (24.21%) | Memory: 84.1 MB (28.26%) - LeetSync

---
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count=0;
+        for(int i : costs){
+            if(i<=coins){
+                coins-=i;
+                count++;
+            }else{
+                return count;
+            }
+        }
+        return count;
+    }
+}
