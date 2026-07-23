27c4fef115464fa597990653bf81170639c65923
Rali Mahi <23MH1A4963@acoe.edu.in>
2026-07-23 14:20:37 +0530

    Time: 1 ms (97.97%) | Memory: 42.7 MB (25.62%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,23 +1,13 @@
 class Solution {
     public int addDigits(int num) {
-        if(num<10){
-            return num;
+        if(num==0){
+            return 0;
         }
-        
-        int res=su(num);
-        while(res>=10){
-            num=res;
-            res=su(num);
-        }
-        return res;
-    }
 
-    public int su(int num){
-        int su=0;
-        while(num>0){
-            su+=num%10;
-            num=num/10;
+        if(num%9==0){
+            return 9;
+        }else{
+            return num%9;
         }
-        return su;
     }
 }
