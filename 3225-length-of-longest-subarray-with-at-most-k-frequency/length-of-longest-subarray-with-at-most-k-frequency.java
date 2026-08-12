caba20d9eab4577a3181ae70d5fae85b0c72c5cc
Rali Mahi <23MH1A4963@acoe.edu.in>
2026-08-12 12:37:38 +0530

    Time: 66 ms (74.64%) | Memory: 89 MB (56.07%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -4,13 +4,12 @@ class Solution {
         int l=0,r=0,maxLen=1;
         while(r<nums.length){
             map.put(nums[r], map.getOrDefault(nums[r],0)+1);
-            if(map.get(nums[r])>k){
                 maxLen=Math.max(maxLen, r-l);
                 while(map.get(nums[r])>k){
                     map.put(nums[l],map.get(nums[l])-1);
                     l++;
                 }
-            }
+                maxLen=Math.max(maxLen, r-l);
             r++;
         }
         maxLen=Math.max(maxLen, r-l);
