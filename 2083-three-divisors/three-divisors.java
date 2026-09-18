84a37b2658531d4c08aff63919faeca928a03d6b
Rali Mahi <23MH1A4963@acoe.edu.in>
2026-09-18 11:26:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (39.55%) - LeetSync

--- 2083-three-divisors/three-divisors.java
+++ 2083-three-divisors/three-divisors.java
@@ -4,9 +4,9 @@ class Solution {
             return false;
         }
         int count=1;
-        for(int i=2;i<=n;i++){
+        for(int i=2;i<=n/2;i++){
             if(n%i==0){
-                count++;
+                count+=2;
             }
             if(count>3){
                 return false;
