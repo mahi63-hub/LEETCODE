b23fc8671c9ff717233626f91152b4c0ff2d9474
Rali Mahi <23MH1A4963@acoe.edu.in>
2026-09-18 11:49:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (20.27%) - LeetSync

--- 2083-three-divisors/three-divisors.java
+++ 2083-three-divisors/three-divisors.java
@@ -1,17 +1,20 @@
 class Solution {
     public boolean isThree(int n) {
-        if(n<=3){
+        int sqrRoot = (int)Math.sqrt(n);
+        if(sqrRoot*sqrRoot!=n || !isPrime(sqrRoot)){
             return false;
         }
-        int count=1;
+        return true;
+    }
+    public boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
         for(int i=2;i<=n/2;i++){
             if(n%i==0){
-                count+=2;
-            }
-            if(count>3){
                 return false;
             }
         }
-        return count==3;
+        return true;
     }
 }
